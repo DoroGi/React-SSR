08535d0de107b06551b716343029fb904927e6eb
Gianmarco Doro <gianmarco.doro@ynap.com>
2017-12-27 16:34:54 +0100

    added bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,90 @@
 const DefinePlugin = require('webpack/lib/DefinePlugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin')
 const webpackNodeExternals = require('webpack-node-externals')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
-const commonConf = {
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            loader: 'awesome-typescript-loader',
-            exclude: /node_modules/
+module.exports = ['client', 'server'].map(target => {
+    const plugins = []
+    const commonConf = {
+        module: {
+            rules: [{
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+                exclude: /node_modules/
+            },
+            {
+                enforce: "pre", test: /\.js$/, loader: "source-map-loader"
+            }]
         },
-        {
-            enforce: "pre", test: /\.js$/, loader: "source-map-loader"
-        }]
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"]
-    }
-}
-
-const byEnvironment = env => {
-    const envConfigs = {
-        development: {
-            devtool: 'inline-source-map',
-            plugins: [ new DefinePlugin({'process.env.NODE_ENV': JSON.stringify('development')})]
-        },
-        production: {
-            devtool: 'source-map',
-            plugins: [
-                new UglifyJSPlugin({ sourceMap: true }),
-                new DefinePlugin({'process.env.NODE_ENV': JSON.stringify('production')})
-            ]
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"]
         }
     }
-    return envConfigs[env] || {}
-}
 
-const byTarget = target => {
-    const config = {
-        client: {
-            name: 'Client',
-            entry: {
-                client: ['./src/client/client.tsx'],
-                vendor: [
-                    'axios',
-                    'react',
-                    'react-dom',
-                    'react-helmet',
-                    'react-redux',
-                    'react-router',
-                    'react-router-config',
-                    'react-router-dom',
-                    'redux',
-                    'redux-thunk',
-                    'serialize-javascript'
-                ]
-            },
-            output: {
-                filename: 'client.js',
-                path: __dirname + '/assets'
-            },
-            plugins: [
-                new CommonsChunkPlugin({
+    const byEnvironment = env => {
+        switch (env) {
+            case 'analyse': 
+                plugins.push(new BundleAnalyzerPlugin({analyzerMode: 'static'}))
+                plugins.push(new DefinePlugin({'process.env.NODE_ENV': JSON.stringify('production')}))
+                return {}
+            case 'development':
+                plugins.push(new DefinePlugin({'process.env.NODE_ENV': JSON.stringify('development')}))
+                return { devtool: 'inline-source-map' }
+            case 'production':
+                plugins.push(new UglifyJSPlugin({ sourceMap: true }))
+                plugins.push(new DefinePlugin({'process.env.NODE_ENV': JSON.stringify('production')}))
+                return { devtool: 'source-map' }
+            default:
+                return {}
+        }
+    }
+
+    const byTarget = target => {
+        switch (target) {
+            case 'client':
+                plugins.push(new CommonsChunkPlugin({
                     name: 'vendor',
                     filename: 'vendor.js',
                     minChunks: Infinity
-                })
-            ]
-        },
-        server: {
-            name: 'Server',
-            target: 'node',
-            entry: './src/index.ts',
-            output: {
-                filename: 'server.js',
-                path: __dirname + '/build'
-            },
-            externals: [webpackNodeExternals()]
-        }
+                }))
+                return {
+                    name: 'Client',
+                    entry: {
+                        client: ['./src/client/client.tsx'],
+                        vendor: [
+                            'axios',
+                            'react',
+                            'react-dom',
+                            'react-helmet',
+                            'react-redux',
+                            'react-router',
+                            'react-router-config',
+                            'react-router-dom',
+                            'redux',
+                            'redux-thunk',
+                            'serialize-javascript'
+                        ]
+                    },
+                    output: {
+                        filename: '[name].js',
+                        path: __dirname + '/assets'
+                    }
+                }
+            case 'server':
+                return {
+                    name: 'Server',
+                    target: 'node',
+                    entry: './src/index.ts',
+                    output: {
+                        filename: 'server.js',
+                        path: __dirname + '/build'
+                    },
+                    externals: [webpackNodeExternals()]
+                }
+            default:
+                return {}
+            }
     }
-    return config[target] || {}
-}
-
-module.exports = ['client', 'server'].map(target => { return {...commonConf, ...byEnvironment(process.env.NODE_ENV), ...byTarget(target)}})
+    return {...commonConf, ...byEnvironment(process.env.NODE_ENV), ...byTarget(target), plugins: plugins}
+})
